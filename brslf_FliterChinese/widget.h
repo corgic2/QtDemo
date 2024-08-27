319cd658c11e03d60769402db57c82aeaa68ef6f
liaofeng <liaofeng@manteiatech.com>
2024-08-27 16:54:57 +0800

    feat:筛选文件夹下所有.cpp和.h的不被tr包括的中文行

--- brslf_FliterChinese/widget.h
+++ brslf_FliterChinese/widget.h
@@ -2,7 +2,9 @@
 #define WIDGET_H
 
 #include <QWidget>
-
+#include <QStandardItemModel>
+#include <QList>
+#include <QTableView>
 namespace Ui {
 class Widget;
 }
@@ -14,9 +16,17 @@ class Widget : public QWidget
 public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
-
+    void SlotGetFileText(const QString& nowPath);
+    void SlotGetFilePath();
+    void CreateItemRow(const QString &text,const QString& textFileName,const QString& textFilePath,const int& rowIndex);
+    void ChangeItemStyle(QStandardItem *item, bool checked);
+    void SlotItemChanged(QStandardItem *item);
+    QStandardItem* CreateItem(const QString& text,int index = 1);
 private:
     Ui::Widget *ui;
+    QList<QString> m_StringListText;
+    QStandardItemModel * m_itemModel = nullptr;
+    QTableView * m_tableView = nullptr;
 };
 
 #endif // WIDGET_H
